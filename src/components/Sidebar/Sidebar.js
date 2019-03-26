b85ec3337f36ae9717c250aed8c960544edcff13
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-27 02:17:22 +0900

    sidebar toggle class적용

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,12 +3,57 @@ import './Sidebar.css';
 import { datas } from '../../datas/Factory';
 
 export default class Sidebar extends Component {
+  componentDidMount() {
+    let menu = document.getElementsByClassName(
+      'leftNavigation__Wrapper-sc-11e7bjo-0'
+    )[0];
+
+    function toggleClass() {
+      let activeSubClass = document.getElementsByClassName('jSbGOb')[0];
+      let activeMainClass = document.getElementsByClassName('SDYKW')[0];
+
+      if (activeMainClass) {
+        activeMainClass.classList.remove('SDYKW');
+        activeMainClass.classList.add('hgGTJo');
+      }
+
+      if (activeSubClass) {
+        activeSubClass.classList.remove('jSbGOb');
+        activeSubClass.classList.add('kdhIax');
+      }
+    }
+
+    menu.addEventListener('click', function(e) {
+      let target = e.target;
+      if (target && target.nodeName === 'DIV') {
+        if (
+          !target.classList.contains('SDYKW') &&
+          target.classList.contains('leftNavigation__LabelSelect-sc-11e7bjo-1')
+        ) {
+          toggleClass();
+
+          target.classList.add('SDYKW');
+        } else if (
+          !target.classList.contains('jSbGOb') &&
+          target.classList.contains('leftNavigation__SubMenu-sc-11e7bjo-2')
+        ) {
+          toggleClass();
+
+          if (target.classList.contains('kdhIax')) {
+            target.classList.remove('kdhIax');
+          }
+          target.classList.add('jSbGOb');
+        }
+      }
+    });
+  }
+
   makeMenu() {
     let row = [];
     for (let data in datas) {
       row.push(
         <div key={data}>
-          <div className="leftNavigation__LabelSelect-sc-11e7bjo-1 SDYKW">
+          <div className="leftNavigation__LabelSelect-sc-11e7bjo-1 hgGTJo">
             {data}
           </div>
           {datas[data].map((value, index) => {
@@ -33,37 +78,6 @@ export default class Sidebar extends Component {
         <div className="leftNavigation__Wrapper-sc-11e7bjo-0 cXdfkr">
           <h3>Menu</h3>
           {this.makeMenu()}
-          {/* <div>
-            <div className="leftNavigation__LabelSelect-sc-11e7bjo-1 hgGTJo">
-              Contract
-            </div>
-          </div>
-          <div>
-            <div className="leftNavigation__LabelSelect-sc-11e7bjo-1 hgGTJo">
-              Factory
-            </div>
-          </div>
-          <div>
-            <div className="leftNavigation__LabelSelect-sc-11e7bjo-1 hgGTJo">
-              Chimney
-            </div>
-          </div>
-          <div>
-            <div className="leftNavigation__LabelSelect-sc-11e7bjo-1 hgGTJo">
-              Para
-            </div>
-          </div>
-          <div>
-            <div className="leftNavigation__LabelSelect-sc-11e7bjo-1 hgGTJo">
-              Data
-            </div>
-            <div className="leftNavigation__SubMenu-sc-11e7bjo-2 jSbGOb">
-              Get Record Data
-            </div>
-            <div className="leftNavigation__SubMenu-sc-11e7bjo-2 kdhIax">
-              Record Data
-            </div>
-          </div> */}
         </div>
       </div>
     );
