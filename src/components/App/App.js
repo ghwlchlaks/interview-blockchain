d855d3097b9ae359ffcf71f12b257ae17b1c016e
ghwlchlaks <ghwlchlaks@naver.com>
2019-03-27 14:35:09 +0900

    테이블 css 추가

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -139,6 +139,37 @@ class App extends Component {
           </p>
         </div>
 
+        <div className="pages__Content-sc-1sxqz4u-1 dVKpwM">
+          <h1 className="label" tabIndex="0">
+            Contract
+          </h1>
+          <div className="code__Wrapper-k9e9h3-0 hWQzFJ">
+            <div className="code__Title-k9e9h3-1 gMMQQA">
+              Create New Web3 Object
+            </div>
+            <div className="code__Type-k9e9h3-2 gyBjzW">Func</div>
+            <div className="code__Method-k9e9h3-3 ksmAft">
+              const web3Object = new Web3(window.web3.currentProvider);
+            </div>
+            <div className="code__Permission-k9e9h3-4 idnNek">
+              Permission :{' '}
+            </div>
+          </div>
+          <div className="code__Wrapper-k9e9h3-0 hWQzFJ">
+            <div className="code__Title-k9e9h3-1 gMMQQA">
+              Create New Contract Object
+            </div>
+            <div className="code__Type-k9e9h3-2 gyBjzW">Func</div>
+            <div className="code__Method-k9e9h3-3 ksmAft">
+              const Contract = new web3Object.eth.Contract(<span>*abi</span>,{' '}
+              <span>*contractAddress</span>);
+            </div>
+            <div className="code__Permission-k9e9h3-4 idnNek">
+              Permission :{' '}
+            </div>
+          </div>
+        </div>
+
         {this.makeContents()}
       </div>
     );
